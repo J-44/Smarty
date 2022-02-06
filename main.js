ea930a94705583eba2d5c619f0e32c2592759cc8
J-44 <74297043+J-44@users.noreply.github.com>
2022-02-06 21:42:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function tick(){
+    var synth = window.speechSynthesis;
+    speak_data = " Settlement is a place where people live and build their homes. There are basically two types of settlements first is temporary settlement and  second one is permanent settlement . In temporary settlement people live in a place for a very short period of time and do work like hunting , gathering, shifting cultivation and transhumance and are mostly founded on the forest areas. In permanent settlement people live in a place for a long period of time and they are mostly founded on cities and towns."
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+function nick(){
+    var ynth = window.speechSynthesis;
+    peak_data = " Urban settlement is mainly a town or a city there most of the people do non-agriculture activities such as car manufacturing ,trading and many more things here there are also some problems that people face are traffic jams and overcrowding because most of the people from poor backgrounds come to cities in search of jobs and if they are lucky so they would lead a good life and if they don not get a good job they live in slums where no good water supply is available , no proper electricity connection is there and the place where they live is unhygienic making them ill."
+    var tterThis = new SpeechSynthesisUtterance(peak_data);
+    ynth.speak(tterThis);
+}
+function lick(){
+    var nth = window.speechSynthesis;
+    eak_data = " Rural settlement is basically a village here houses are made according to climatic conditions of that place. Some examples of this are the place which receives heavy rainfall have slanting roof , places where water accumulates they construct their houses on platforms or slits to protect their houses from water and people living in hot areas make there houses from locally available materials like mud ,stones ,clay , straw etc. The people here depend on agriculture , forestry , fishing, crafts work and trading. Here there are two types of settlements first one is scattered settlement and second one is compact settlement. In scattered settlement between houses there is a lot of place these houses are mostly founded on hilly tracts, thick forests and regions of extreme climate."
+    var terThis = new SpeechSynthesisUtterance(eak_data);
+    nth.speak(terThis);
+}
+function sick(){
+    var th = window.speechSynthesis;
+    ak_data = " The people of past lived in temporary settlement because they used to hunt and gather food ,as all things of that place got over they moved to another place in search of food. When they learnt the farming technique they lived in permanent settlement because growing crop is a long term process so they started building huts and they mostly did farming near river because the soil over there was fertile."
+    var erThis = new SpeechSynthesisUtterance(ak_data);
+    th.speak(erThis);
+}
+function cick(){
+    var h = window.speechSynthesis;
+    k_data = " Transhumance : In this people who rear animals start searching for pastures according to changes in season. Site : It is a place where a building or a settlement develops. Conditions for an Site are : .1 favourable climate .2 availability of water .3 suitable land .4 fertile soil"
+    var rThis = new SpeechSynthesisUtterance(k_data);
+    h.speak(rThis);
+}
+function cick(){
+    var h = window.speechSynthesis;
+    k_data = " The second type of settlement is compact settlement in this houses are built close to each other and this is founded on plain land."
+    var rThis = new SpeechSynthesisUtterance(k_data);
+    h.speak(rThis);
+}
+
